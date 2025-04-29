fe3eac53c71c5d4c03b96ada4ea2cf270fc2b8c4
Patrick Tennant <pstennant@gmail.com>
2025-04-29 15:42:33 -0500

    app functioning with traffic level, need to work on pop ups and hover text

--- app.R
+++ app.R
@@ -1,30 +1,36 @@
 library(tidyverse)
 library(mapgl)
 library(mapboxapi)
-library(mapdeck)
 library(tidycensus)
 library(sf)
 library(shiny)
 library(shinythemes)
+library(leaflet)
+library(htmltools)
 
 # Loading tract data
-tx_tract_mhv <- readRDS("data/tx_tract_mhv.rds")
+tract_data <- readRDS("data/tract_data.rds")
+tract_data <- st_as_sf(tract_data)
 
 # Define UI for application
 ui <- fluidPage(
   titlePanel("Texas Drive Time Neighborhood Analysis"),
-  theme = shinytheme("spacelab"),  # Add theme
+  theme = shinytheme("darkly"), # Add theme
   sidebarLayout(
     sidebarPanel(
+      width = 3,
+      style = "height: 100vh;",
       textInput("address", "Enter Address"),
-      sliderInput("minutes", "Select Drive Time (Minutes)", 
-                  min = 0, max = 60, value = 15, step = 1, "Shiny"),
-      actionButton("submit", "Submit")
+      sliderInput("minutes", "Select Drive Time (Minutes)",
+        min = 1, max = 60, value = 15, step = 1, "Shiny"
       ),
-
+      selectInput("traffic", "Traffic Level", choices = c("Low", "Moderate", "High"), selected = "Moderate"),
+      actionButton("submit", "Submit")
+    ),
     mainPanel(
-      textOutput("output"),
-      mapdeckOutput("map")
+      width = 9, height = "100vh",
+      style = "height: 100vh;",
+      leafletOutput("map", height = "100%")
     )
   )
 )
@@ -32,33 +38,62 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   df <- eventReactive(input$submit, {
-    isochrone <-  mb_isochrone(
-        input$address,
-        profile = "driving",
-        time = input$minutes)
-    
-    isochrone_tracts <- sf::st_intersection(isochrone, st_transform(tx_tract_mhv, 4326))
+    if (input$traffic == "Low") {
+      depart_at_var <- "2025-03-01T08:00"
+    }
+
+    if (input$traffic == "Moderate") {
+      depart_at_var <- "2025-03-03T14:00"
+    }
+
+    if (input$traffic == "High") {
+      depart_at_var <- "2025-03-05T17:30"
+    }
+
+    isochrone <- mb_isochrone(
+      input$address,
+      profile = "driving",
+      time = input$minutes,
+      depart_at = depart_at_var
+    )
+
+    isochrone_tracts <- sf::st_intersection(isochrone, st_transform(tract_data, 4326))
 
     return(isochrone_tracts)
   })
 
-  output$map <- renderMapdeck({
+  output$map <- renderLeaflet({
     req(df())
-    mapdeck(style = mapdeck_style("light")) %>%
-      add_polygon(
-        data = df(), 
-        fill_colour = "estimate",
-        fill_opacity = .75,
-        palette = grDevices::colorRamp(c("#EAEAEA", 
-        "#293064"))( (1:256)/256 ))
-    })
-  
-  output$output <- renderText({
-    req(df())
-    paste("You entered:", input$address, "address and", input$minutes, "minutes")
+    # req(df()) is a reactive expression that returns NULL if df() is not available,
+    # and the value of df() if it is. This is used to prevent the map from rendering
+    # until the user has submitted an address and a drive time.
+    pal <- colorRampPalette(c("#CCCCCC", "#293064"))
+    df() %>%
+      select(
+        census_tract,
+        estimate = acs_median_home_value,
+        geometry
+      ) %>%
+      drop_na() %>%
+      leaflet() %>%
+      addProviderTiles(providers$CartoDB.DarkMatter) %>%
+      addPolygons(
+        fillColor = ~ pal(100)[as.numeric(cut(estimate, breaks = 100, labels = FALSE))],
+        weight = .1,
+        opacity = 1,
+        color = "black",
+        fillOpacity = 0.75,
+        highlightOptions = highlightOptions(weight = .4),
+        label = ~paste0(
+          "Census Tract: ", census_tract
+        ),
+        popup = ~paste0(
+          "Census Tract: ", census_tract,
+          ", Median Home Value: ", estimate
+        )
+      )
   })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
