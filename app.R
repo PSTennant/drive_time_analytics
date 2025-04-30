f761922fcac357e2d6ac998421df3fc1c66f9a01
Patrick Tennant <pstennant@gmail.com>
2025-04-30 08:09:50 -0500

    functioning app with metric selection, still needs polishing

--- app.R
+++ app.R
@@ -21,10 +21,21 @@ ui <- fluidPage(
       width = 3,
       style = "height: 100vh;",
       textInput("address", "Enter Address"),
-      sliderInput("minutes", "Select Drive Time (Minutes)",
+      sliderInput(
+        "minutes", "Select Drive Time (Minutes)",
         min = 1, max = 60, value = 15, step = 1, "Shiny"
       ),
       selectInput("traffic", "Traffic Level", choices = c("Low", "Moderate", "High"), selected = "Moderate"),
+      selectInput(
+        "metric", "Metric",
+        choices = c(
+          "hmda_tract_median_age_of_housing_units",
+          "acs_median_gross_rent" ,
+          "acs_median_home_value",
+          "hmda_median_loan_amount"
+        ),
+        selected = "Home Value (ACS)"
+      ),
       actionButton("submit", "Submit")
     ),
     mainPanel(
@@ -38,6 +49,23 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   df <- eventReactive(input$submit, {
+
+    # if (input$metric == "Age of Housing Units (HMDA)") {
+    #   metric_var <- "hmda_tract_median_age_of_housing_units"
+    # }
+
+    # if (input$metric == "Gross Rent (ACS)") {
+    #   metric_var <- "acs_median_gross_rent" 
+    # }
+
+    # if (input$metric == "Home Value (ACS)") {
+    #   metric_var <- "acs_median_home_value"
+    # }
+
+    # if (input$metric == "Median Loan Amount (HMDA)") {
+    #   metric_var <- "hmda_median_loan_amount"
+    # }
+
     if (input$traffic == "Low") {
       depart_at_var <- "2025-03-01T08:00"
     }
@@ -70,8 +98,8 @@ server <- function(input, output) {
     pal <- colorRampPalette(c("#CCCCCC", "#293064"))
     df() %>%
       select(
-        census_tract,
-        estimate = acs_median_home_value,
+        census_tract, # Using the rlang package to use the value of metric_var,
+        estimate = input$metric,
         geometry
       ) %>%
       drop_na() %>%
@@ -84,10 +112,10 @@ server <- function(input, output) {
         color = "black",
         fillOpacity = 0.75,
         highlightOptions = highlightOptions(weight = .4),
-        label = ~paste0(
+        label = ~ paste0(
           "Census Tract: ", census_tract
         ),
-        popup = ~paste0(
+        popup = ~ paste0(
           "Census Tract: ", census_tract,
           ", Median Home Value: ", estimate
         )
